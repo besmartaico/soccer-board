a788d395fededf40bf5b08e9b7f7f277d5b4a2cd
besmartaico <besmartai.co@gmail.com>
2026-03-18 14:37:15 -0600

    Fix: add missing UserRole import to share page

--- app/app/share/[token]/page.tsx
+++ app/app/share/[token]/page.tsx
@@ -1,6 +1,7 @@
 "use client";
 import { useEffect, useMemo, useRef, useState } from "react";
 import { HtmlBoard, type PlacedPlayer, type BoardObject, type BoardTool } from "@/lib/board/HtmlBoard";
+import type { UserRole } from "@/lib/roles";
 
 
 
